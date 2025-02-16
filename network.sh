3e71209b390d85772a27becf891a2a26fc0d9f93
Masaru Tsuchiyama <m.tmatma@gmail.com>
2025-02-16 14:49:09 +0900

    remove ipv4

--- network.sh
+++ network.sh
@@ -4,6 +4,4 @@ docker network create -d macvlan \
   --ipv6 \
   --subnet 2001:db8:100::/64 \
   --gateway 2001:db8:100::1 \
-  --subnet=192.168.11.0/24 \
-  --gateway=192.168.11.1 \
   -o parent=enp86s0 macvlan
