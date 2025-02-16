9932e82b28f4420de9b6169a20c22a55907985cc
Masaru Tsuchiyama <m.tmatma@gmail.com>
2025-02-16 15:24:03 +0900

    インターフェースインデックスを引数で渡す

--- multicast_sender.py
+++ multicast_sender.py
@@ -1,20 +1,29 @@
 #!/usr/bin/python3
 import socket
 import time
+import sys
 
-MCAST_GROUP = 'ff02::1'
-MCAST_PORT = 12345
-INTERFACE_NAME = 'enp86s0'  # 使用するインターフェースを指定（例: eth0, wlan0 など）
+def loop(multicast_index: int):
+    MCAST_GROUP = 'ff02::1'
+    MCAST_PORT = 12345
 
-# IPv6 ソケットを作成
-sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, 1)
+    # IPv6 ソケットを作成
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, 1)
 
-# 使用するインターフェースを設定
-sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_IF, socket.if_nametoindex(INTERFACE_NAME))
+    # 使用するインターフェースを設定
+    sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_IF, multicast_index)
 
-while True:
-    message = b"Hello, Multicast World!"
-    sock.sendto(message, (MCAST_GROUP, MCAST_PORT))
-    print(f'Sent message: {message}')
-    time.sleep(1)
+    while True:
+        message = b"Hello, Multicast World!"
+        sock.sendto(message, (MCAST_GROUP, MCAST_PORT))
+        print(f'Sent message: {message}')
+        time.sleep(1)
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print('Usage: python3 multicast_sender.py <multicast_index>')
+        sys.exit(1)
+
+    multicast_index = int(sys.argv[1])
+    loop(multicast_index)
