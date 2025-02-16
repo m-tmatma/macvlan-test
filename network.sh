34855dd89a1bbbe0859eb1fa4cb82629a543a2b7
Masaru Tsuchiyama <m.tmatma@gmail.com>
2025-02-16 16:29:23 +0900

    detect network interface

--- network.sh
+++ network.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 
+# ネットワークインターフェース名を検出
+if ip addr show dev enp86s0 >/dev/null 2>&1; then
+    INTERFACE="enp86s0"
+elif ip addr show dev eth0 >/dev/null 2>&1; then
+    INTERFACE="eth0"
+else
+    echo "エラー: 必要なネットワークインターフェースが見つかりません"
+    exit 1
+fi
+
 docker network create -d macvlan \
   --ipv6 \
   --subnet 2001:db8:100::/64 \
   --gateway 2001:db8:100::1 \
-  -o parent=enp86s0 macvlan
+  -o parent=$INTERFACE macvlan
